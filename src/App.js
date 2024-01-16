5562080d65d4bd31a34d3d03eae82102301f21f9
Duncan Collins <75388877+duncan222@users.noreply.github.com>
2024-01-16 13:33:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-import React, { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
