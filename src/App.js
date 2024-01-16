ec20bc13bb6b6077e436b89278adc9969606b995
Duncan Coll <duncancollins101@gmail.com>
2024-01-16 00:50:02 -0500

    changes fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import React, { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
